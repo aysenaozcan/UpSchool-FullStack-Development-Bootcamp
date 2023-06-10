0cd777e0ef659c9984184780515dc369f3e3e24c
aysenaozcan <92749952+aysenaozcan@users.noreply.github.com>
2023-06-10 23:35:12 +0300

    ödev5
    .

---
+++ ödev5/ToDo/src/App.tsx
@@ -0,0 +1,16 @@
+import {Todolist} from "./Todolist";
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="App"><Todolist/> 
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
